aae0a061c53230baa01e05093a8c36bf5942d3ee
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-02-14 20:26:00 +0000

    Add script

---
+++ scripts/free.sh
@@ -0,0 +1,50 @@
+ID=$(pvesh get /cluster/nextid)
+PASSWORD=$1
+IP=$2
+echo "Creating your vps..."
+
+sleep 1
+
+echo "ID-$(echo $ID)"
+
+sleep 1
+
+pct create $ID /var/lib/vz/template/cache/debian-12-standard_12.2-1_amd64.tar.zst --hostname=free$ID --swap=4096 --memory=4096 --cmode=shell --net0 name=eth0,bridge=vmbr0,firewall=1,gw=10.5.0.1,ip=$IP/16,rate=5 --ostype=debian --password $PASSWORD --start=1 --unprivileged=1 --cores=1 --features fuse=1,nesting=1,keyctl=1
+
+echo Created
+
+echo "Setting up firewall..."
+
+cp /etc/pve/firewall/100.fw /etc/pve/firewall/$ID.fw
+
+echo "Enabling ssh..."
+pct exec $ID bash -- -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
+
+pct reboot $ID
+
+PN="/ports/$(echo $ID).sh"
+
+echo "# VM $ID" > $PN
+echo "# SSH" >> $PN
+echo "iptables -t nat -A PREROUTING -p TCP --dport 3$(echo $ID)0 -j DNAT --to-destination $(echo $IP):22" >> $PN
+echo "# forward ports" >> $PN
+for i in {1..9}
+do
+   echo "iptables -t nat -A PREROUTING -p TCP --dport 3$(echo $ID)$(echo $i) -j DNAT --to-destination $(echo $IP):3$(echo $ID)$(echo $i)" >> $PN
+   echo "iptables -t nat -A PREROUTING -p UDP --dport 3$(echo $ID)$(echo $i) -j DNAT --to-destination $(echo $IP):3$(echo $ID)$(echo $i)" >> $PN
+done
+
+echo "echo forwarded ports for $(echo $ID)" >> $PN
+echo "" >> $PN
+
+echo "pct fstrim $ID" >> ../trim.sh
+
+echo "Forwarding ports..."
+
+# sleep 1
+
+bash $PN
+
+# clear
+echo "Done"
+# echo now redo bfport.sh
