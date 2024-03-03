d6b639df24654475b12c9bedd527a348385296c2
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-03-03 10:23:47 +0000

    Format

--- index.js
+++ index.js
@@ -14,58 +14,58 @@ const slashCtrl = new SlashCtrl({
 
 slashCtrl.publishCommandsFromFolder(path.join(__dirname, 'cmd'));
 
-const { Client, GatewayIntentBits } = require( 'discord.js' );
+const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-    
+    console.log(`Logged in as ${client.user.tag}!`);
+
     const db = require('./cmd/db');
-    
+
     var isChecking;
     isChecking = false;
-    
+
     setInterval(async () => {
         log('> Checking vps expire');
-        
+
         if (isChecking == true) {
             return console.log('already checking expire');
         }
-       
+
         isChecking = true;
-        
-         var vps = await db.VPS.find({
+
+        var vps = await db.VPS.find({
             expiry: {
                 $lt: Date.now()
             }
         });
-        
-        for(let ab = 0; ab < vps.length; ab++) {
+
+        for (let ab = 0; ab < vps.length; ab++) {
             var expiredVPS = vps[ab];
             const channel = client.channels.cache.get('1204045694164533269');
             if (!expiredVPS.proxID) {
                 console.log(`vps ${expiredVPS._id} does not have proxmox id`, expiredVPS);
-                
+
                 channel.send(`<@${expiredVPS.userID}> Your vps failed to create and has been removed from the database. Try again with /create in #cmds`);
-                
+
                 await db.VPS.deleteMany({ _id: expiredVPS._id });
-                
+
             } else {
-            
-            await exec('bash /root/remove.sh ' + expiredVPS.proxID);
-            
-			channel.send(`VPS ${expiredVPS.proxID} expired and was deleted. <@${expiredVPS.userID}>`);
-            
-            await db.VPS.deleteMany({ proxID: expiredVPS.proxID });
+
+                await exec('bash /root/remove.sh ' + expiredVPS.proxID);
+
+                channel.send(`VPS ${expiredVPS.proxID} expired and was deleted. <@${expiredVPS.userID}>`);
+
+                await db.VPS.deleteMany({ proxID: expiredVPS.proxID });
             }
         }
-        
+
         isChecking = false;
         console.log('> Expiry check done!')
-        
+
         // console.log('List', vps);
-    }, 60*1000);
-    
+    }, 60 * 1000);
+
 });
 
 client.login(botToken);
