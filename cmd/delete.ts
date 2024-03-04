8df2fbd62e9f6a6a6604ea927d16a7aee44d1b21
JustADeve <142085654+JustADeve@users.noreply.github.com>
2024-03-04 16:25:29 +0100

    Update delete.ts

--- cmd/delete.ts
+++ cmd/delete.ts
@@ -1,77 +1,46 @@
-const db = require('./db');
 const { SlashCommand } = require('slashctrl');
+const db = require('./db');
 
-class CMD extends SlashCommand {
+class DeleteCommand extends SlashCommand {
+  constructor() {
+    super();
+    this.guilds = ['1013767249884094534', '1203730039296888842'];
+    this.setName('delete');
+    this.setDescription('Deletes your VPS');
+    this.addIntegerOption(option =>
+      option.setName('id').setDescription('VPS ID (found in /list)').setRequired(true)
+    );
+    this.addBooleanOption(option =>
+      option.setName('sure').setDescription('Are you sure you want to delete your VPS?').setRequired(true)
+    );
+  }
 
-    constructor() {
-        super();
-        
-        this.guilds = ["1013767249884094534", "1203730039296888842"];
-        
-        this.setName("delete");
-        this.setDescription("Deletes your vps");
-        
-        this.addIntegerOption(option =>
-            option.setName('id')
-                .setDescription('VPS ID (found in /list)')
-				.setRequired(true));
-        
-        this.addBooleanOption(option =>
-            option.setName('sure')
-                .setDescription('Are you sure you want to delete your vps?')
-				.setRequired(true));
+  async execute(ints) {
+    const userID = ints.user.id;
+    const user = await db.User.findOne({ userID });
+    if (!user) {
+      user = new db.User({ userID, balance: 1, plan: 'free' });
+      await user.save();
     }
-    
-    async execute(ints) {
-        
-        var userID = ints.user.id;
-        
-        var user = await db.User.findOne({
-            userID: userID
-        });
-        
-        if (!user) {
-            user = new db.User({
-                userID,
-                balance: 1,
-                plan: 'free'
-            });
-            await user.save();
-        }
-        
-        var id = ints.options.getInteger('id');
-        var sure = ints.options.getBoolean('sure');
-        
-        if (!sure) return ints.reply(`Cancelled destroy. User is not sure.`);
-        
-        var vps = await db.VPS.findOne({
-            proxID: id
-        });
-        
-        if (!vps) {
-            return await ints.reply(`[1] VPS ${id} not found`);
-        }
-        
-        if (vps.userID != ints.user.id) {
-            if (userID != '554344892827172884') {
-            	return await ints.reply(`[2] No access to VPS ${id} - ${vps.userID} / ${userID}`);
-            }
-        }
-        
-        
-        const exec = require('./ssh');
-        
-        await ints.deferReply();
-        
-       await exec(`bash /root/remove.sh ${id}`);
-
-      	var dbDel = await db.VPS.deleteOne({ _id: vps._id });
-        
-        console.log('delete', dbDel);
-        
-        ints.editReply(`VPS destroyed`);
+    const id = ints.options.getInteger('id');
+    const sure = ints.options.getBoolean('sure');
+    if (!sure) return ints.reply(`Cancelled destroy. User is not sure.`);
+    const vps = await db.VPS.findOne({ proxID: id });
+    if (!vps) {
+      return await ints.reply(`[1] VPS ${id} not found`);
     }
-
+    if (vps.userID !== ints.user.id) {
+      if (userID !== '554344892827172884') {
+        return await ints.reply(`[2] No access to VPS ${id} - ${vps.userID} / ${userID}`);
+      }
+    }
+    const exec = require('./ssh');
+    await ints.deferReply();
+    await exec(`bash /root/remove.sh ${id}`);
+    const dbDel = await db.VPS.deleteOne({ _id: vps._id });
+    console.log('delete', dbDel);
+    ints.editReply(`VPS destroyed`);
+  }
 }
 
-module.exports = { default: CMD };
+module.exports = { default: DeleteCommand };
